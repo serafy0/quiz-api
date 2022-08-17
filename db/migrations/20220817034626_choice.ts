109f2a9bdeed1fc8f89247bdb5f0d411672a5047
mohamedelserafy <elserafy02@gmail.com>
2022-08-17 11:39:09 +0200

    choice migration

---
+++ db/migrations/20220817034626_choice.ts
@@ -0,0 +1,15 @@
+import { Knex } from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("choices", (table) => {
+        table.increments("id").primary()
+        table.integer("number").notNullable()
+        table.enum("type", ["text", "picture"]).defaultTo("text")
+        table.string("picture")
+        table.uuid("question_id").references("questions.id").onDelete("CASCADE")
+    })
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists("choices")
+}
