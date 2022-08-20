d49d0fa71881938c7a97d860c1027a4075ad5254
mohamedelserafy <elserafy02@gmail.com>
2022-08-20 09:02:13 +0200

    validaton errors

--- middleware/errorHandling/errorHandler.ts
+++ middleware/errorHandling/errorHandler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express"
+import { ValidationError } from "objection"
 import ApiError from "./ApiError"
 
 function errorHandler(
@@ -8,13 +9,17 @@ function errorHandler(
     next: NextFunction
 ) {
     const isProduction: boolean = process.env.NODE_ENV === "production"
-    // return res.status(err.status).json({ error: err.message })
+
+    if (err instanceof ValidationError) {
+        return res.status(err.statusCode).json({ message: err.message })
+    }
+
     if (!err.status) {
         err.message = isProduction ? "something went wrong" : err.message
         err.status = 500
     }
 
-    return res.status(err.status).json({ error: err.message })
+    return res.status(err.status).json({ message: err.message })
 }
 
 export default errorHandler
