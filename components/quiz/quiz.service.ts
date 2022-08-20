84f748125e55e8a053a77ff8035640c77c189064
mohamedelserafy <elserafy02@gmail.com>
2022-08-20 19:30:04 +0200

    edit quiz service

--- components/quiz/quiz.service.ts
+++ components/quiz/quiz.service.ts
@@ -3,3 +3,11 @@ import Quiz from "./quiz.model"
 export async function addQuiz(title: string, picture: string) {
     return await Quiz.query().insert({ title, picture }).returning("*").first()
 }
+
+export async function editQuiz(id: string, title: string, picture: string) {
+    return await Quiz.query().patchAndFetchById(id, { title, picture })
+}
+
+export async function getOneQuiz(id: string) {
+    return await Quiz.query().findById(id)
+}
