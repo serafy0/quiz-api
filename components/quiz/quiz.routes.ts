08bb5c02f90b54afc284be06b36663483feb8588
mohamedelserafy <elserafy02@gmail.com>
2022-08-20 19:31:02 +0200

    patch quiz route

--- components/quiz/quiz.routes.ts
+++ components/quiz/quiz.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
-import { createQuiz } from "./quiz.controller"
+import { createQuiz, updateQuiz } from "./quiz.controller"
 const router = Router()
 
 router.post("/", createQuiz)
+router.patch("/:id", updateQuiz)
 
 export default router
